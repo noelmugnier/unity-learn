a3218139a1aaf8486ca8a9fb1dd98f8d4746a01b
Noël Mugnier <noel.mugnier@outlook.com>
2023-07-05 18:27:08 +0200

    Refactored constraint on player movement

--- Prototype2/Assets/Scripts/PlayerController.cs
+++ Prototype2/Assets/Scripts/PlayerController.cs
@@ -27,21 +27,34 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         var direction = _actions.Player.Move.ReadValue<Vector2>();
-        var x = direction.x;
-        if (transform.position.x < -15 && x < 0)
-            return;
-        
-        if (transform.position.x > 15 && x > 0)
-            return;
-        
-        var y = direction.y;
-        if (transform.position.z < 0 && y < 0)
-            return;
-        
-        if (transform.position.z > 10 && y > 0)
-            return;
-        
-        var movementDirection = new Vector3(x, 0f, y);
+        direction = ConstraintHorizontalMovement(direction);
+        direction = ConstraintVerticalMovement(direction);
+
+        var movementDirection = new Vector3(direction.x, 0f, direction.y);
         transform.Translate(movementDirection * (Time.deltaTime * movementSpeed));
+    }
+
+    private Vector2 ConstraintVerticalMovement(Vector2 direction)
+    {
+        direction.y = transform.position.z switch
+        {
+            < 0 when direction.y < 0 => 0,
+            > 10 when direction.y > 0 => 0,
+            _ => direction.y
+        };
+
+        return direction;
+    }
+
+    private Vector2 ConstraintHorizontalMovement(Vector2 direction)
+    {
+        direction.x = transform.position.x switch
+        {
+            < -15 when direction.x < 0 => 0,
+            > 15 when direction.x > 0 => 0,
+            _ => direction.x
+        };
+
+        return direction;
     }
 }
